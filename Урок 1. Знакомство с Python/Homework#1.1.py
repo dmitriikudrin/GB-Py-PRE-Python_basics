c3e6856b98aa4039f4075d67348740fd6a9d5161
dmitriikudrin <39126785+dmitriikudrin@users.noreply.github.com>
2019-10-18 11:09:43 +0700

    change ex1 les1

--- Урок 1. Знакомство с Python/Homework#1.1.py
+++ Урок 1. Знакомство с Python/Homework#1.1.py
@@ -1,31 +1,29 @@
 """
-Я уверен, что эту задачу можно написать как-то изящнее, но пока не знаю как.
-Буду рад получить рекомендации:)
+Пользователь вводит время в секундах. Переведите время в часы, минуты и
+секунды и выведите в формате чч:мм:сс. Используйте форматирование строк.
 """
 
-seconds = input("Введите время в секундах - ")
+print("Для выхода из программы введите exit.")
+user_data = input("Введите время в секундах - ")
 while True:
-    if seconds.isdigit():
-        seconds = int(seconds)
+    # Условие выхода из цикла
+    if user_data.lower() == "exit":
+        print("Работа программы завершена.")
         break
+    #
+    elif user_data.isdigit():
+        # Вычисление резульатата
+        user_data = int(user_data)
+        hours = user_data // 3600
+        minutes = (user_data % 3600) // 60
+        seconds = user_data % 60
+        # Вывод результата
+        print()
+        print("Итоговое время:")
+        print(f"{hours:02}:{minutes:02}:{seconds:02}")
+        break
+    # Повторный ввод в случае неверного ввода
     else:
-        seconds = input("Неверный ввод. Необходимо ввести целое число - ")
+        user_data = input("Неверный ввод. Необходимо ввести целое число - ")
 
-h = seconds // 3600
-m = (seconds % 3600) // 60
-s = seconds % 60
 
-h_str = str(h)
-m_str = str(m)
-s_str = str(s)
-
-if len(h_str) == 1:
-    h_str = '0' + h_str
-if len(m_str) == 1:
-    m_str = '0' + m_str
-if len(s_str) == 1:
-    s_str = '0' + s_str
-
-print()
-print("Итоговое время:")
-print("%s:%s:%s" % (h_str, m_str, s_str))
