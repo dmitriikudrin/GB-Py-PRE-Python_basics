787c43ce0372ca0488a14949b4f02aa9aedc9296
dmitriikudrin <39126785+dmitriikudrin@users.noreply.github.com>
2019-10-21 17:45:58 +0700

    some changes les2

--- Урок 1. Знакомство с Python/Homework#1.2.py
+++ Урок 1. Знакомство с Python/Homework#1.2.py
@@ -1,11 +1,27 @@
+"""
+Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
+Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+"""
+
+
 # Входные данные
-n = input("Введите число n - ")
+print("Для выхода из программы введите exit.")
+user_number = input("Введите число n - ")
+
 while True:
-    if n.isdigit():
+    # Условие выхода из цикла
+    if user_number.lower() == "exit":
+        print("Работа программы завершена.")
+        break
+    #
+    elif user_number.isdigit():
+        user_number = int(user_number)
+        # Вывод результата
+        print()
+        print(f"Сумма n + nn + nnn - "
+              f"{user_number + user_number * 2 + user_number * 3}")
         break
     else:
-        n = input("Неверный ввод. Необходимо ввести целое число - ")
+        user_number = input("Неверный ввод. Необходимо ввести целое число - ")
 
-# Вывод результата
-print()
-print(f"Сумма n + nn + nnn - {int(n) + int(n * 2) + int(n * 3)}")
+
